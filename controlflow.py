604b3fd687396007a9a7978f46dbc167d6cdefb5
User 01 <user01@unst.(none)>
2017-02-28 17:03:16 +0000

    day one of python

---
+++ controlflow.py
@@ -0,0 +1,41 @@
+'''#creates a while loop
+print  'before'
+num_moons = 3
+while num_moons > 0:
+	print num_moons
+	num_moons-=1
+print 'after' '''
+
+'''#if and elif. Always start with if. can have any number of elif
+moons = 3
+if moons <0:
+	print 'less'
+elif moons == 0:
+	print 'equal'
+else:
+	print 'greater' '''
+
+#double indentation: a while followed by an if :)
+'''num=0
+while num<=10:
+	if (num %2) ==1:
+		print num
+	num +=1 '''
+#excercise 2 using while again. note how counter and print are inside the loop
+''' mylist = [23, "hi", 2.4e-10]
+counter = 0
+
+while counter <  3:
+    item=mylist[counter]
+    counter +=1
+    print item
+'''
+
+#excercise 3 
+
+x=[1,2]
+if x: 
+    print x, "is True"
+ 
+
+
